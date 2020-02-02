2f41eae72e3c2bf6d4d527e9bfcacfcdeefd19b9
Bday7593 <43048009+Bday7593@users.noreply.github.com>
2020-02-02 16:51:21 -0500

    changed the name of function to test_store_data():
    wrote function test_write_jobs_to_file() that tests for matching job companies.
    
    added Github Jobs.txt which is tha save file destination.

--- GithubJobsAPI.py
+++ GithubJobsAPI.py
@@ -1,13 +1,8 @@
 # Brian Day
 # Comp 490 - Development Seminar
 import json
+
 import requests
-
-
-# going through each json page.
-def data_retrieval(url):
-    raw_datum = requests.get(url)  # requesting the URL and saving it as a data type
-    return raw_datum
 
 
 def store_data(data):
@@ -18,20 +13,12 @@ def store_data(data):
         jobs_list.append(item)
 
 
-def page_is_full(items_on_the_page):
-    if items_on_the_page == 50:
-        print("PAGE NOT FULL = 0")
-        return 1
-    else:
-        return 0
-
-
 def github_jobs_search():
     page = 1  # the page the url search is on.
     is_page_full = 1
     while is_page_full == 1:
         url1 = 'https://jobs.github.com/positions.json?page=' + str(page)  # re-assign URL for Github Jobs.txt
-        raw_data = data_retrieval(url1)
+        raw_data = requests.get(url1)  # requesting the URL and saving it as a data type
         print("PAGE = " + str(page))
         store_data(raw_data)
         if len(jobs_list) % 50 != 0:
@@ -40,15 +27,11 @@ def github_jobs_search():
             page = page + 1
 
 
+# takes a list of dictionaries and saved them to a text file
 def write_jobs_to_file(basic_list):
     print("WITHIN write_jobs_to_file()")
-    with open('Github Jobs.txt', 'w') as output:
-        json.dump(basic_list, output)
-        # for item in basic_list:
-        # json.dump(item, output)
-        # output.write(json.dump(item, output))
-        # output.write(json.dumps(basic_list))
-        print("WITHIN write_jobs_to_file() TRYING TO WRITE TO FILE")
+    with open('Github Jobs.txt', 'w') as output:  # open the file and close it when actions are done.
+        json.dump(basic_list, output)  # write the list of dictionaries to the file
 
 
 jobs_list = []  # defining a list to store the items from the json dictionary.
@@ -57,6 +40,8 @@ write_jobs_to_file(jobs_list)
 
 counts = 0
 for job in jobs_list:
-    # print(job['company'])
+    print(job['company'])
+    # print(job['location'])
+    # print()
     counts = counts + 1
 print(str(counts))
--- tests/GithubJobsTests.py
+++ tests/GithubJobsTests.py
@@ -1,9 +1,30 @@
 # Brian Day
 # Comp 490 - Development Seminar
+import json
 
 import GithubJobsAPI
 
 
-#def test_list_length():
-# if len(GithubJobsAPI.jobs_list) >= 100:
+# one test should the method that retrieves the data from the web and assure that you get more than 100 data items
+def test_store_data():
+    assert len(GithubJobsAPI.jobs_list) >= 100, "Needs to be >= 100"
 
+
+# The second test should check to see that the save file contains a job with a job title that you know should be there.
+def test_write_jobs_to_file():  # F. Hoffmann-La Roche AG
+    with open('Github Jobs.txt', 'r') as output:  # open the file and close it when actions are done.
+        job_list = json.load(output)    # load the json into a list
+        for item in job_list:   # cycle though the list and search for a matching job title.
+            if item['company'] == 'F. Hoffmann-La Roche AG':    # if it matches then the test was a success
+                assert item['company'] == 'F. Hoffmann-La Roche AG', "There was no matching job title"
+
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+
+if __name__ == "__main__":
+    test_sum()
+    test_store_data()
+    test_write_jobs_to_file()
+    print("Everything passed")
